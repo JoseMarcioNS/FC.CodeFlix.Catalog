94d3e36f05b64de56cc16ab3be49d750255efc4e
JoseMarcioNS <63798521+JoseMarcioNS@users.noreply.github.com>
2022-09-19 17:11:16 -0300

    Refatorado as Fixures dos casos de usos da Categoria

--- tests/FC.CodeFlix.Catalog.UnitTests/Application/UseCases/Category/Delete/DeleteCategoryTestFixture.cs
+++ tests/FC.CodeFlix.Catalog.UnitTests/Application/UseCases/Category/Delete/DeleteCategoryTestFixture.cs
@@ -1,5 +1,6 @@
 ﻿using FC.CodeFlix.Catalog.Application.Interfaces;
 using FC.CodeFlix.Catalog.Domain.Interfaces;
+using FC.CodeFlix.Catalog.UnitTests.Application.UseCases.Category.Common;
 using FC.CodeFlix.Catalog.UnitTests.Common;
 
 namespace FC.CodeFlix.Catalog.UnitTests.Application.UseCases.Category.Delete
@@ -8,30 +9,6 @@ namespace FC.CodeFlix.Catalog.UnitTests.Application.UseCases.Category.Delete
     public class DeleteCategoryTestFixureCollection : ICollectionFixture<DeleteCategoryTestFixture>
     {}
 
-    public class DeleteCategoryTestFixture : BaseFixture
-    {
-        public Mock<ICategoryRepository> GetCategoryRepositoryMock() => new();
-        public Mock<IUnitOfWork> GetUnitOfWorkMock() => new();
-
-        public string GetValidName()
-        {
-            var name = "";
-            while (name.Length < 3)
-                name = Faker.Commerce.Categories(1)[0];
-
-            if (name.Length > 255)
-                name = name[..255];
-
-            return name;
-        }
-        public string GetValidDescription()
-        {
-            var description = Faker.Commerce.ProductDescription();
-            if (description.Length > 10_000)
-                description = description[..10_000];
-
-            return description;
-        }
-        public DomainEntity.Category GetValidCategory() => new(GetValidName(), GetValidDescription());
-    }
+    public class DeleteCategoryTestFixture : CategoryBaseFixture
+    {}
 }
--- tests/FC.CodeFlix.Catalog.UnitTests/Application/UseCases/Category/List/ListCategoriesTestFixture.cs
+++ tests/FC.CodeFlix.Catalog.UnitTests/Application/UseCases/Category/List/ListCategoriesTestFixture.cs
@@ -1,36 +1,13 @@
 ﻿using FC.CodeFlix.Catalog.Application.UseCases.Category.List;
-using FC.CodeFlix.Catalog.Domain.Interfaces;
 using FC.CodeFlix.Catalog.Domain.SeedWork.SearchableRepository;
-using FC.CodeFlix.Catalog.UnitTests.Common;
+using FC.CodeFlix.Catalog.UnitTests.Application.UseCases.Category.Common;
 
 namespace FC.CodeFlix.Catalog.UnitTests.Application.UseCases.Category.List
 {
     [CollectionDefinition(nameof(ListCategoriesTestFixture))]
     public class ListCategoriesTestFixtureCollection : ICollectionFixture<ListCategoriesTestFixture> { }
-    public class ListCategoriesTestFixture : BaseFixture
+    public class ListCategoriesTestFixture : CategoryBaseFixture
     {
-        public Mock<ICategoryRepository> GetCategoryRepositoryMock() => new();
-
-        public string GetValidName()
-        {
-            var name = "";
-            while (name.Length < 3)
-                name = Faker.Commerce.Categories(1)[0];
-
-            if (name.Length > 255)
-                name = name[..255];
-
-            return name;
-        }
-        public string GetValidDescription()
-        {
-            var description = Faker.Commerce.ProductDescription();
-            if (description.Length > 10_000)
-                description = description[..10_000];
-
-            return description;
-        }
-        public DomainEntity.Category GetValidCategory() => new(GetValidName(), GetValidDescription());
         public List<DomainEntity.Category> ListCategories(int length = 10)
         {
             List<DomainEntity.Category> categories = new List<DomainEntity.Category>();
@@ -47,7 +24,7 @@ namespace FC.CodeFlix.Catalog.UnitTests.Application.UseCases.Category.List
                  perPage: radom.Next(15, 50),
                  search: Faker.Commerce.ProductName(),
                  sort: Faker.Commerce.ProductName(),
-                 dir: radom.Next(1,5) > 5 ?  SearchOrder.Asc : SearchOrder.Desc
+                 dir: radom.Next(1, 5) > 5 ? SearchOrder.Asc : SearchOrder.Desc
                  );
         }
     }
